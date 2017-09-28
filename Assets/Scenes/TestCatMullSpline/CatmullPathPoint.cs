6a6e8c72f7bed26591fcd34e253501ff34a3f629
Erric <mateixzxf@icloud.com>
2017-09-28 09:55:03 +0800

    设置测试场景

---
+++ Assets/Scenes/TestCatMullSpline/CatmullPathPoint.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Assets.Scenes.TestCatmull
+{
+    public class CatmullPathPoint : MonoBehaviour
+    {
+
+        public enum eCatmullPointType
+        {
+            ePoint_Area1,
+            ePoint_Area2,
+        }
+
+        public eCatmullPointType PointType = eCatmullPointType.ePoint_Area1;
+
+
+        void OnDrawGizmos()
+        {
+
+            switch (PointType)
+            {
+                case eCatmullPointType.ePoint_Area1:
+                    {
+                        Gizmos.color = Color.green;
+                        break;
+                    }
+                case eCatmullPointType.ePoint_Area2:
+                    {
+                        Gizmos.color = Color.cyan;
+                        break;
+                    }
+            }
+           
+
+            Gizmos.DrawWireSphere(transform.position, .5f);
+        }
+
+    }
+}
+
---
+++ Assets/Scenes/TestCatMullSpline/Control.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scenes.TestCatmull
+{
+
+ 
+
+    public class Control : MonoBehaviour
+    {
+
+        public CatmullPathPoint[] Area1;
+        public CatmullPathPoint[] Area2;
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
